6cce834bf8647c3d79368a9071ccc9e60e9c3f87
Tomas Mendoza <albertomendozainf@gmail.com>
2024-12-24 07:44:36 +0100

    wrangler define

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,6 +15,15 @@ export default defineConfig({
     ssr: {
       target: "webworker",
       external: ["node:crypto", "node:path"]
+    },
+    define: {
+      'process.env.TURSO_DATABASE_URL': JSON.stringify(process.env.TURSO_DATABASE_URL),
+      'process.env.TURSO_AUTH_TOKEN': JSON.stringify(process.env.TURSO_AUTH_TOKEN),
+      'process.env.RECAPTCHA_SECRET': JSON.stringify(process.env.RECAPTCHA_SECRET),
+      'process.env.PUSHER_APP_ID': JSON.stringify(process.env.PUSHER_APP_ID),
+      'process.env.PUBLIC_PUSHER_KEY': JSON.stringify(process.env.PUBLIC_PUSHER_KEY),
+      'process.env.PUSHER_SECRET': JSON.stringify(process.env.PUSHER_SECRET),
+      'process.env.PUBLIC_PUSHER_CLUSTER': JSON.stringify(process.env.PUBLIC_PUSHER_SECRET)
     }
   }
 });
